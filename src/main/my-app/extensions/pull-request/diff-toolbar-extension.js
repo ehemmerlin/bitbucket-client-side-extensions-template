734da903e38f970050b91934e2b3bb6b4002093e
Maciej Adamczak <madamczak@atlassian.com>
2020-07-17 09:05:51 +1000

    Add example of modal extensions

--- src/main/my-app/extensions/pull-request/diff-toolbar-extension.js
+++ src/main/my-app/extensions/pull-request/diff-toolbar-extension.js
@@ -1,15 +1,41 @@
-import { ButtonExtension } from '@atlassian/clientside-extensions';
+import { ModalExtension } from '@atlassian/clientside-extensions';
 
 /**
  * @clientside-extension
  * @extension-point bitbucket.ui.pullrequest.diff.toolbar
  */
-export default ButtonExtension.factory((pluginApi, context) => {
+export default ModalExtension.factory((pluginApi, context) => {
+    // A glyph name from '@atlaskit/icon/glyph/jira/labs';
+    // Check the AK docs for more icons https://atlaskit.atlassian.com/packages/core/icon
+    const iconAfter = 'jira/labs';
+
     return {
         hidden: false,
+        iconAfter,
         label: 'My diff toolbar extension',
-        onAction: () => {
-            console.log('My diff toolbar extension', context);
+        onAction: modalApi => {
+            modalApi.onMount(container => {
+                modalApi.setTitle('My diff toolbar extension!');
+
+                modalApi.setActions([
+                    {
+                        text: 'Close',
+                        onClick: () => {
+                            modalApi.closeModal();
+                        },
+                    },
+                ]);
+
+                container.innerHTML = `
+            		<div>
+            			<h3>Hello there!</h3>
+            			<p>Here is the context</p>
+        				<pre style="white-space: pre-wrap; word-break: break-all;">
+        					${JSON.stringify(context, null, '  ')}
+    					</pre>
+    				</div>
+				`;
+            });
         },
     };
 });
--- src/main/my-app/extensions/pull-request/overview-summary-extension.js
+++ src/main/my-app/extensions/pull-request/overview-summary-extension.js
@@ -5,8 +5,13 @@ import { ButtonExtension } from '@atlassian/clientside-extensions';
  * @extension-point bitbucket.ui.pullrequest.overview.summary
  */
 export default ButtonExtension.factory((pluginApi, context) => {
+    // A glyph name from '@atlaskit/icon/glyph/add-circle';
+    // Check the AK docs for more icons https://atlaskit.atlassian.com/packages/core/icon
+    const iconBefore = 'add-circle';
+
     return {
         hidden: false,
+        iconBefore,
         label: 'My overview extension',
         onAction: () => {
             console.log('My overview extension', context);
