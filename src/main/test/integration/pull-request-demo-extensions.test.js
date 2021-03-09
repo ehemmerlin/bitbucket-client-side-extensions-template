44cacec2d6c86f9866c909778ef5f2f6b0bff350
Maciej Adamczak <madamczak@atlassian.com>
2021-03-10 09:28:26 +1100

    CSE-86: Add more integration tests for the extensions

--- src/main/test/integration/pull-request-demo-extensions.test.js
+++ src/main/test/integration/pull-request-demo-extensions.test.js
@@ -26,13 +26,48 @@ describe('Pull Request demo extensions', () => {
         done();
     });
 
-    it('should render extension for "bitbucket.ui.pullrequest.overview.summary" extension point', async () => {
+    it('should render a new button for the "bitbucket.ui.pullrequest.overview.summary" extension point', async () => {
+        // given
+        const extensionLabel = 'My overview extension';
+
+        // when
         const summaryPanel = await page.$('.summary-panel');
         await page.waitForSelector('.demo-overview-extension');
-        const demoExtension = await findElementByText(summaryPanel, 'My overview extension');
+        const extensionButton = await findElementByText(summaryPanel, extensionLabel);
 
-        expect(demoExtension).toBeTruthy();
+        // then
+        expect(extensionButton).toBeTruthy();
     });
 
-    it.skip('should navigate to the custom page', async () => {});
+    it('should render an option item for the "bitbucket.ui.pullrequest.action" extension point', async () => {
+        const extensionLabel = 'My pull request action extension';
+
+        // when
+        // Click on more actions and select
+        const moreActionsButton = await page.$('[data-testid="more-actions--trigger"]');
+        await moreActionsButton.click();
+        await page.waitForSelector('[data-testid="more-actions--content"]');
+
+        const moreActionsOptions = await page.$('[data-testid="more-actions--content"]');
+        const option = await findElementByText(moreActionsOptions, extensionLabel);
+
+        // then
+        expect(option).toBeTruthy();
+    });
+
+    it('should render a new button item for the "bitbucket.ui.pullrequest.diff.toolbar" extension point', async () => {
+        const extensionLabel = 'My diff toolbar extension';
+
+        // when
+        const diffTab = await page.$('[data-testid="tab-DIFF"]');
+        await diffTab.click();
+        // Wait for change toolbar to be rendered
+        await page.waitForSelector('.diff-toolbar-extension');
+
+        const diffToolbar = await page.$('.diff-toolbar-extension');
+        const extensionButton = await findElementByText(diffToolbar, extensionLabel);
+
+        // then
+        expect(extensionButton).toBeTruthy();
+    });
 });
